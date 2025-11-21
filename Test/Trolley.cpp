782cfd96fd75ea62f9a5a0b65aafd2baa751495a
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2025-11-21 11:46:37 +0900

    トロッコのレールカメラスピード部分

--- Test/Trolley.cpp
+++ Test/Trolley.cpp
@@ -2,6 +2,11 @@
 
 #include "Framework/AssetManager.h"
 
+#ifdef _DEBUG
+#include "Graphics/ImGuiManager.h"
+#endif // _DEBUG
+
+
 Trolley::Trolley()
 {
 	model_.SetModel(AssetManager::GetInstance()->modelMap.Get("trolley")->Get());
@@ -13,16 +18,76 @@ void Trolley::Initialize()
 	transform_.translate += offset_;
 	transform_.UpdateMatrix();
 	model_.SetWorldMatrix(transform_.worldMatrix);
+
+	maxTrollySpeed_ = 1.0f;
+	trollySpeed_ = 1.0f;
+	trollyDeceleration_ = 0.001f;
+	trollyAcceleration_ = 0.001f;
+	trollyMaxFillUpTime_ = 30;
+	trollyFillUpTime_ = trollyMaxFillUpTime_;
 }
 
 void Trolley::Update()
 {
-	transform_.translate += offset_;
+
+	TrollySpeedUpdate();
+
 	transform_.UpdateMatrix();
 	model_.SetWorldMatrix(transform_.worldMatrix);
+
+#ifdef _DEBUG
+	ImGui::Begin("GameScene");
+	if (ImGui::TreeNode("Troller")) {
+		if (ImGui::TreeNode("Troller")) {
+			ImGui::DragFloat3("Offset", &offset_.x, 0.01f);
+			ImGui::TreePop();
+		}
+
+		if (ImGui::TreeNode("TrollerSpeed")) {
+
+			ImGui::SliderFloat("CurrentTrollySpeed", &trollySpeed_, 0.0f, maxTrollySpeed_, "Speed: %.2f");
+			ImGui::SliderInt("CurrentFillUpTime", &trollyFillUpTime_, 0, trollyFillUpTime_, "Time: %d");
+
+			ImGui::Separator();
+
+			ImGui::DragFloat("MaxTrollySpeed", &maxTrollySpeed_, 0.01f);
+			ImGui::DragFloat("Deceleration", &trollyDeceleration_, 0.001f);
+			ImGui::DragFloat("Acceleration", &trollyAcceleration_, 0.001f);
+			ImGui::DragInt("MaxFillUpTime", &trollyFillUpTime_, 1);
+			ImGui::TreePop();
+		}
+
+		ImGui::TreePop();
+	}
+	ImGui::End();
+#endif // _DEBUG
+
+}
+
+void Trolley::TrollySpeedUpdate()
+{
+	//スピードがMaxだった場合
+	if (trollyFillUpTime_ <= 0) {
+		trollySpeed_ -= trollyDeceleration_;
+		trollySpeed_ = std::clamp(trollySpeed_, 0.0f, maxTrollySpeed_);
+	}
+	else {
+		trollyFillUpTime_--;
+	}
+}
+
+void Trolley::OnCollision()
+{
+	trollySpeed_ += trollyAcceleration_;
+	trollySpeed_ = std::clamp(trollySpeed_, 0.0f, maxTrollySpeed_);
+	//スピードがMaxかどうか
+	if (trollySpeed_ >= maxTrollySpeed_) {
+		trollyFillUpTime_ = trollyMaxFillUpTime_;
+	}
 }
 
 void Trolley::SetTransform(const Transform& transform)
 {
 	transform_ = transform;
+	transform_.translate += offset_;
 }
--- Test/Trolley.h
+++ Test/Trolley.h
@@ -10,12 +10,26 @@ public:
 
 	void Initialize();
 	void Update();
+	void TrollySpeedUpdate();
+
+	void OnCollision();
 
 	void SetTransform(const Transform& transform);
+	float GetTrollySpeed() const { return trollySpeed_; }
 private:
 	ModelInstance model_;
 
 	Transform transform_;
 
 	Vector3 offset_;
+
+#pragma region トロッコスピード関連
+	float maxTrollySpeed_;
+	float trollySpeed_;
+	float trollyDeceleration_;
+	float trollyAcceleration_;
+	//MaxSpeed時のスピード維持時間
+	int trollyFillUpTime_;
+	int trollyMaxFillUpTime_;
+#pragma endregion
 };
